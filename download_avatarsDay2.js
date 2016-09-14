b6c13483ec55df09e5d5a2922004e8693198bd8e
bchuup <bchuup@gmail.com>
2016-09-14 02:46:13 +0000

    tried to implement function that creates env file if there are none... still working on it

--- download_avatarsDay2.js
+++ download_avatarsDay2.js
@@ -1,20 +1,7 @@
 //Q for code review: why do we declare variables for require?
-var fs = require("fs")
 var request = require("request")
 var func = require("./functions.js")
-
-
-fs.exists('.env', (exists) => {
-  if(!exists){
-    fs.writeFile(".env", "I just made you an .env file to place your token \n ddfa  ", function(err) {
-      if(err) {
-          return console.log(err);
-      }
-      console.log("I just made you an .env file, which you will need in order to proceed! \n Please enter the following inside the file: \n token = '\Your Token'\ ")
-    });
-  }
-});
-
+var fs = require("fs")
 var key = require('dotenv').config()
 token = process.env.token
 // console.log(token)
@@ -34,18 +21,21 @@ function getRepoContributors(repoOwner, repoName, callback) {
     requestData = func.auth('https://api.github.com', repoOwner, repoName)
     request.get(requestData, function(err, response, body) {
         if (err) {
-          callback(err);
+          throw (err);
+        }
+        var data = JSON.parse(body)
+        if (data.message === "Bad credentials"){
+            throw new Error('You just played yourself. Make sure your token is correct \n Check out the .env file');
         }
         if (!(response.headers.status === "200 OK")) {
             throw new Error('You just played yourself. Check if your github repo or github user really exists');
         } else {
-          console.log("print pictures")
-          // var data = JSON.parse(body)
-          // var urlList = [] 
-          // for (var everyObject of data){
-          //   urlList.push(everyObject.avatar_url)
-          // }
-          //   callback(null, urlList)
+          // console.log("print pictures")
+          var urlList = [] 
+          for (var everyObject of data){
+            urlList.push(everyObject.avatar_url)
+          }
+            callback(null, urlList)
         }
     })
 }
@@ -61,7 +51,18 @@ getRepoContributors(process.argv[2], process.argv[3], func.manyPrints)
 
 
 
-
+// Still working on function to create env file when there is none
+// var makeEnv = function(){
+//   fs.exists('.env', (exists) => {
+//     if(!exists){
+//       fs.writeFile(".env", "Please enter the following \n token = '\ Your Token'\  ", function(err) {
+//         if(err) {
+//           return console.log(err);
+//         }
+//       })
+//     }
+//   })
+// }
 
 
 
